c81a3028fa8f0ae8afd53494c0e0259677738487
operah24 <66219854+operah24@users.noreply.github.com>
2020-09-17 22:19:18 +0100

    Added html

--- script.js
+++ script.js
@@ -170,7 +170,7 @@ jQuery(function() {
 
         static clearField(){
             $('#describe-input').val('');
-            $('#assign-input').val('');
+            $('#assign-input').val('Choose Assignee...');
             $('#inputGroupSelect03').val('Choose Severity...');
         }
     };
